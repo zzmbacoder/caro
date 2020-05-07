07e1579c645af25f24adf97aab30c20c525c7f0a
Zhaoyuan Zhang <zhaoyuanzhang1120@gmail.com>
2020-05-07 14:58:47 -0400

    Switched to use logo instead of text in the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,50 +10,59 @@ import {
 } from "reactstrap";
 
 class Footer extends React.Component {
-  render() {
-    return (
-      <footer className=" footer">
-        <Container>
-          <Row className=" align-items-center justify-content-md-between">
-            <Col md="6">
-                <div className="footerCopyRightText">
-                    © {new Date().getFullYear()}{" "}
-                    CBS Academic Rep Online (CARO)
-                </div>
-            </Col>
-            <Col md="6">
-              <Nav className=" nav-footer justify-content-end">
-                <NavItem>
-                  <NavLink
-                    href="https://github.com/zzmbacoder/caro"
-                    target="_blank"
-                  >
-                    About CARO
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    href=""
-                    target="_blank"
-                  >
-                    MIT License
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    href="https://courseworks2.columbia.edu/"
-                    target="_blank"
-                  >
-                    Canvas
-                  </NavLink>
-                </NavItem>
-              </Nav>
-            </Col>
-          </Row>
-        </Container>
-      </footer>
-    );
-  }
+    statics = {
+        gitRepoLink: 'https://github.com/zzmbacoder/caro',
+        canvasLink: 'https://courseworks2.columbia.edu'
+    };
+
+    render() {
+        return (
+        <footer className=" footer">
+            <Container>
+            <Row className=" align-items-center justify-content-md-between">
+                <Col md="6">
+                    <div className="footerCopyRightText">
+                        © {new Date().getFullYear()}{" "}
+                        CBS Academic Rep Online (CARO)
+                    </div>
+                </Col>
+                <Col md="6">
+                <Nav className=" nav-footer justify-content-end">
+                    <NavItem>
+                    <NavLink
+                        href={this.statics.gitRepoLink}
+                        target="_blank"
+                    >
+                        <span className="btn-inner--icon">
+                            <img
+                            alt="..."
+                            className="footerLogo"
+                            src={require("../assets/img/icons/common/github.svg")}
+                            />
+                        </span>
+                    </NavLink>
+                    </NavItem>
+                    <NavItem>
+                    <NavLink
+                        href={this.statics.canvasLink}
+                        target="_blank"
+                    >
+                        <span className="btn-inner--icon">
+                            <img
+                            alt="..."
+                            className="footerLogo"
+                            src={require("../assets/img/icons/common/canvas.png")}
+                            />
+                        </span>
+                    </NavLink>
+                    </NavItem>
+                </Nav>
+                </Col>
+            </Row>
+            </Container>
+        </footer>
+        );
+    }
 }
 
 export default Footer;
